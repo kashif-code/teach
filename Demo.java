1e4c3113e55e609177bd055fedb7baf9f61c8247
kashif-code <kashif1234504@gmail.com>
2019-09-27 10:39:06 +0530

    first commit

---
+++ Demo.java
@@ -0,0 +1,13 @@
+
+public class Demo {
+   public static void main(String []args) {
+      String str = "9as78";
+      int count = 0;
+      System.out.println("String: "+str);
+      for (int i = 0; i < str.length(); i++) {
+         if (Character.isLetter(str.charAt(i)))
+            count++;
+      }
+      System.out.println("Letters: "+count);
+   }
+}
